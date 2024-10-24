b2276abc367754de85d0c713c0952025a887eabc
Johan Held <johan.held@postnord.com>
2024-10-24 11:10:52 +0200

    Added scheduled scraper for cph half

---
+++ functions/cph-half-scraper/index.py
@@ -0,0 +1,65 @@
+import boto3
+import os
+import requests
+from bs4 import BeautifulSoup
+
+TICKET_URL = 'https://secure.onreg.com/onreg2/bibexchange/?eventid=6591&language=us'
+SNS_ARN = os.environ['SNS_ARN']
+
+def lambda_handler(event, context):
+    print('-----------handler started------------')
+    
+    available_ticket, status = check_tickets()
+    
+    if status:
+        subject = 'AVAILABLE' if available_ticket else 'IN PROGRESS'
+        publish_to_sns(subject)
+    
+    return {
+        'statusCode': 200,
+        'body': f'tickets available: {available_ticket}'
+    }
+
+def check_tickets():
+    response = requests.get(TICKET_URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # For local test files
+    # with open('sample-pending.html', 'r', encoding='utf-8') as file:
+    #     html_content = file.read()
+
+    # soup = BeautifulSoup(html_content, 'html.parser')
+
+    page_text = soup.get_text()
+
+    if "There are currently no race numbers for sale" in page_text:
+        print("No tickets available")
+        return False, False 
+    if "In progress" in page_text:
+        print("Ticket in progress")
+        return False, True
+
+    print("Text not found the page. Tickets available!")
+    print(page_text)
+    return True, True
+
+def publish_to_sns(status):
+    client = boto3.client('sns')
+    
+    subject = f'TICKETS {status}'
+    message = f'Tickets are {status.lower()}.\nCheck the link: {TICKET_URL}'
+
+    response = client.publish(
+        TopicArn= SNS_ARN,
+        Message=message,
+        Subject=subject
+    )
+
+    print('-------------------------')
+    print("Message published to SNS:", response['MessageId'])
+            
+if __name__ == '__main__':
+    event = {}  # Provide any necessary event data here
+    context = {}  # Provide any necessary context data here
+    result = lambda_handler(event, context)
+    print(result)
--- web_scraper/web_scraper_stack.py
+++ web_scraper/web_scraper_stack.py
@@ -37,6 +37,10 @@ class WebScraperStack(Stack):
             topic_name="article-topic"
         )
 
+        ticket_topic = Topic(self, "TicketTopic",
+            topic_name="ticket-topic"
+        )
+
         chrome_driver_layer = LayerVersion(
             self, 'ChromeDriverLayer',
             compatible_runtimes=[Runtime.PYTHON_3_8],
@@ -73,14 +77,37 @@ class WebScraperStack(Stack):
             }
         )
 
+        cph_half_scraper_function=lambda_alpha_.PythonFunction(
+            self, 'CphHalfFunction',
+            function_name='cph-half-scraper',
+            runtime=Runtime.PYTHON_3_8,
+            handler='lambda_handler', 
+            entry='./functions/cph-half-scraper',
+            memory_size=512,
+            timeout=Duration.minutes(1),
+            environment={
+                'SNS_ARN': ticket_topic.topic_arn,
+            }
+        )
+
         rule = aws_events.Rule(
             self, 'DailyLambdaEvent',
             schedule=aws_events.Schedule.cron(hour='4', minute='0'),)
         
+        ticket_rule = aws_events.Rule(
+            self, 'TicketLambdaEvent',
+            schedule=aws_events.Schedule.cron(
+                minute='0/1',
+                hour='0-1,6-23'
+            )
+        ) 
+
         rule.add_target(aws_events_targets.LambdaFunction(sellpy_scraper_function))
         rule.add_target(aws_events_targets.LambdaFunction(vinted_scraper_function))
+        ticket_rule.add_target(aws_events_targets.LambdaFunction(cph_half_scraper_function))
 
         table.grant_full_access(sellpy_scraper_function)
         table_vinted.grant_full_access(vinted_scraper_function)
         topic.grant_publish(sellpy_scraper_function)
         topic.grant_publish(vinted_scraper_function)
+        ticket_topic.grant_publish(cph_half_scraper_function)
